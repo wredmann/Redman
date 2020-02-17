f0bac562371a26ff0b0d0041c1f9ef26735fdfe7
wredmann <59452780+wredmann@users.noreply.github.com>
2020-02-17 21:37:01 +0100

    music app

---
+++ index.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector(".visual");
+    const colors = [
+        "#60d394",
+        "#d36860",
+        "#d36099",
+        "#bad360",
+        "#6260d3",
+        "#60a9d3"
+    ];  
+    
+
+    // Lets get the sound
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', function() {
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+
+            createBubbles(index);
+        });
+    });
+
+    //create a function that makes bubbles
+    const createBubbles = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = "jump 1s ease";
+        bubble.addEventListener("animationend", function() {
+            visual.removeChild(this);
+        })
+    }
+});
